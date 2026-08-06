2ef0ffe9d7b876a937e64a9abeb76937eb411159
Avadhut kapurkar <avadhutkapurkar@gmail.com>
2026-08-06 21:02:05 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (50.26%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -4,13 +4,12 @@ class Solution {
             int product = 1;
             int x = n;
 
-            // Calculate product of digits
+         
             while (x > 0) {
                 product *= (x % 10);
                 x /= 10;
             }
 
-            // Check divisibility
             if (product % t == 0) {
                 return n;
             }
